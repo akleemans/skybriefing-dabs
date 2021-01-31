468a91031a93e9feadcd13a39b84fa23ebf5f127
Adrianus Kleemans <a.kleemans@gmail.com>
2021-01-31 21:14:22 +0100

    Update script to Python3

--- README.md
+++ README.md
@@ -3,55 +3,9 @@
 Automated script to download PDFs from [skybriefing.com](https://www.skybriefing.com/) and send them via email.
 Uses [Selenium](http://www.seleniumhq.org/) for simulating a browser, so the PDFs get created properly.
 
-## Installation
+The old script runs on Python 2 and is in `py2`.
 
-### Mac OS X, Windows 7, Ubuntu
-* Install Firefox via official Website
-* Install Selenium: `pip install selenium`
-* Install Geckodriver: `brew install geckodriver`
+The download of the PDF has been ported to Python 3 and can be run with:
 
-(Trust certificates: `pip --trusted-host pypi.org --trusted-host files.pythonhosted.org install pyvirtualdisplay selenium`)
+    python3 get_pdfs.py
 
-Additional step for Raspberry Pi: `sudo apt-get install iceweasel xvfb`. If you don't have `pip` installed on Windows, see [here](http://stackoverflow.com/q/4750806).
-
-Copy the scripts to a folder, configure them as mentioned below and then run them each to check if they work on your system.
-
-* `get_pdfs.py` simulates the browser to download the PDFs
-* `send_email.py` sends the email via a gmail account
-* `skybriefing_dabs.sh` is wrapper to execute both scripts, run this via cron
-
-## Configuration
-
-### Downloading PDFs
-
-At the beginning of the script `get_pdfs.py` there is a config section in which you can specify login and some other data. The only mandatory parameter is `WITH_LOGIN`, set this to `True` if you have a login, or `False` if you just want the DABS report. If set to False, all other parameters will be ignored.
-
-    WITH_LOGIN = True # Use script with login? if true, fill out info below
-    USER = 'my_login'
-    PW = 'my_password'
-    FLUGPLATZ = 'LSZH'
-    LOWER_FL = '000'
-    UPPER_FL = '120'
-
-### E-Mail
-
-In `send_email.py`, you'll have to configure sender and recipient email addresses. Sender Email must be gmail, don't use your real address here.
-Also make sure to allow ["access for less secure apps"](https://www.google.com/settings/security/lesssecureapps) to enable sending emails via script.
-
-    SEND_TO = ['myemail1@gmail.com', 'myemail2@gmail.com']
-    PDFS = 1 # set to 2 if you used a login on other script
-    PDF_PATH = 'my path' # Mac OS X: '~/temp/', or on Raspi: '/home/pi/temp/'
-    SENDER_EMAIL = 'sender-email@gmail.com'
-    SENDER_PW = 'gmail-password'
-    EMAIL_SUBJECT = 'skybriefing.com report'
-    EMAIL_BODY = 'Sent from Raspberry Pi :)'
-    
-*Important*: Make sure the directory matches where the files are downloaded and that is temporary! The script will delete *all* files in the directory to clean up!
-
-### Automate (Linux / Mac only)
-Make file executable: `chmod +x skybriefing_dabs.sh`
-
-Install crontab: Use `crontab -e` to open crontab.
-Add the following entry:
-
-    0 7 * * * ~/path/to/your/script/skybriefing_dabs.sh`
--- get_pdfs.py
+++ get_pdfs.py
@@ -1,73 +1,53 @@
-import selenium.webdriver as webdriver
-from pyvirtualdisplay import Display
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 import time
 
-### CONFIG START ###
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
 
 USER = 'my_login'
 PW = 'my_password'
-COUNTRY ='Switzerland'
-FLUGPLATZ = 'LSZH'
+COUNTRY = 'Switzerland'
+FLUGPLATZ = 'LSZB'
 LOWER_FL = '000'
-UPPER_FL = '120'
-LOG_FILE = 'log.txt'
+UPPER_FL = '080'
 
-### CONFIG END ###
-
-log_file = open(LOG_FILE, 'a')
-
-def log(msg):
-    """Logs message and writes it to logfile"""
-    print msg
-    log_file.write(msg + '\n')
-
-
-log('Starting...')
-log('Setting up virtual display and driver...')
-display = Display(visible=0, size=(800, 600))
-display.start()
-
-log('Setting up Firefox...')
+print('Starting. Setting up Firefox...')
 profile = webdriver.FirefoxProfile()
 profile.set_preference('browser.download.folderList', 2)
 profile.set_preference('browser.download.manager.showWhenStarting', False)
-profile.set_preference('browser.download.dir', "~/temp/")
+# profile.set_preference('browser.download.dir', "~/temp/")
 profile.set_preference('browser.helperApps.neverAsk.saveToDisk', ('application/pdf'))
 profile.set_preference('pdfjs.disabled', True)
 driver = webdriver.Firefox(profile)
 
-log('Opening website...')
+print('Opening website...')
 driver.get("https://www.skybriefing.com")
 time.sleep(10)
 
-## download DABS PDF
-log('Downloading first PDF...')
+print('Downloading first PDF...')
 element = driver.find_elements_by_css_selector('div[location="pdfLinkToday"] a')[0]
 element.click()
 time.sleep(10)
 
-log('Logging in...')
+print('Logging in...')
 driver.get("https://www.skybriefing.com/de/signinnativ")
 time.sleep(40)
-log('Entering username...')
+print('Entering username...')
 element = driver.find_elements_by_xpath('//*[@id="_com_liferay_login_web_portlet_LoginPortlet_login"]')[0]
 element.send_keys(USER)
 time.sleep(5)
-log('Entering password...')
+print('Entering password...')
 element = driver.find_elements_by_xpath('//*[@id="_com_liferay_login_web_portlet_LoginPortlet_password"]')[0]
 element.send_keys(PW)
 time.sleep(5)
 element.send_keys(Keys.ENTER)
 time.sleep(5)
-log('Open Area Briefing...')
+print('Open Area Briefing...')
 driver.get('https://www.skybriefing.com/de/new-area-briefing')
 time.sleep(30)
 
 # Information Area
 element = Select(driver.find_elements_by_css_selector('div.v-select:nth-child(1) > select:nth-child(1)')[0])
-#element.select_by_visible_text("Switzerland")
 element.select_by_visible_text(COUNTRY)
 time.sleep(2)
 
@@ -77,30 +57,29 @@ element.send_keys(FLUGPLATZ)
 time.sleep(2)
 
 # lower FL
-element = driver.find_elements_by_css_selector('#gwt-uid-5')[0]
+element = driver.find_elements_by_css_selector('#gwt-uid-6')[0]
 element.send_keys(LOWER_FL)
 time.sleep(2)
 
 # upper FL
-element = driver.find_elements_by_css_selector('#gwt-uid-15')[0]
+element = driver.find_elements_by_css_selector('#gwt-uid-14')[0]
 element.send_keys(UPPER_FL)
 time.sleep(2)
 
-log('Entered flight information and pressed Enter, this could take a while...')
+print('Entered flight information and pressed Enter, this could take a while...')
 element.send_keys(Keys.ENTER)
 time.sleep(70)
 
-log('Downloading generated PDF...')
+print('Downloading generated PDF...')
 element = driver.find_elements_by_css_selector('div.skb:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > span:nth-child(2)')[0]
 element.click()
 time.sleep(20)
 
 # logging out
-log('Logging out...')
+print('Logging out...')
 driver.get('https://www.skybriefing.com/c/portal/logout')
 time.sleep(10)
 
 # clean quit
-driver.quit()
-log('All done!')
-log_file.close()
+driver.close()
+print('All done!')
